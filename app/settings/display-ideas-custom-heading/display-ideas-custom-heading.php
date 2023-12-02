79c1e1ff41f5fa087a1a3f9e6e1ffbfc137da11e
James Welbes <james.welbes@gmail.com>
2023-12-02 11:31:55 -0600

    hide custom heading field if hide heading checkbox is checked

--- app/settings/display-ideas-custom-heading/display-ideas-custom-heading.php
+++ app/settings/display-ideas-custom-heading/display-ideas-custom-heading.php
@@ -22,12 +22,39 @@ add_filter('wp_roadmap_custom_display_ideas_heading_text', function($default_hea
 // Filter for adding new heading text field in settings
 add_filter('wp_roadmap_hide_display_ideas_heading_setting', function($content) {
     $pro_options = get_option('wp_roadmap_pro_settings', []);
-    $hide_submit_idea_heading_checked = isset($pro_options['hide_display_ideas_heading']) ? 'checked' : '';
-    $new_submit_idea_heading = isset($pro_options['custom_display_ideas_heading']) ? $pro_options['custom_display_ideas_heading'] : '';
+    $hide_display_ideas_heading_checked = isset($pro_options['hide_display_ideas_heading']) ? 'checked' : '';
+    $new_display_ideas_heading = isset($pro_options['custom_display_ideas_heading']) ? $pro_options['custom_display_ideas_heading'] : '';
 
-    $content = '<input type="checkbox" name="wp_roadmap_pro_settings[hide_display_ideas_heading]" value="1" ' . $hide_submit_idea_heading_checked . ' />';
-    $content .= '<br/><label for="custom_display_ideas_heading">New Heading:</label>';
-    $content .= '<input type="text" name="wp_roadmap_pro_settings[custom_display_ideas_heading]" value="' . esc_attr($new_submit_idea_heading) . '" />';
+    $content = '<input type="checkbox" name="wp_roadmap_pro_settings[hide_display_ideas_heading]" id="hide_display_ideas_heading" value="1" ' . $hide_display_ideas_heading_checked . ' />';
+    $content .= '<br/>';
+
+    // Add JavaScript to toggle the visibility of the label and input field based on checkbox state
+    $content .= '<script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var checkbox = document.getElementById("hide_display_ideas_heading");
+            var label = document.querySelector("label[for=custom_display_ideas_heading]");
+            var input = document.querySelector("input[name=\'wp_roadmap_pro_settings[custom_display_ideas_heading]\']");
+
+            checkbox.addEventListener("change", function() {
+                if (checkbox.checked) {
+                    label.style.display = "none";
+                    input.style.display = "none";
+                } else {
+                    label.style.display = "inline";
+                    input.style.display = "inline";
+                }
+            });
+
+            // Initialize visibility based on the initial checkbox state
+            if (checkbox.checked) {
+                label.style.display = "none";
+                input.style.display = "none";
+            }
+        });
+    </script>';
+
+    $content .= '<label for="custom_display_ideas_heading">New Heading:</label>';
+    $content .= '<input type="text" name="wp_roadmap_pro_settings[custom_display_ideas_heading]" value="' . esc_attr($new_display_ideas_heading) . '" />';
 
     return $content;
 });
--- app/settings/submit-idea-custom-heading/submit-idea-custom-heading.php
+++ app/settings/submit-idea-custom-heading/submit-idea-custom-heading.php
@@ -25,8 +25,35 @@ add_filter('wp_roadmap_hide_custom_idea_heading_setting', function($content) {
     $hide_submit_idea_heading_checked = isset($pro_options['hide_custom_idea_heading']) ? 'checked' : '';
     $new_submit_idea_heading = isset($pro_options['custom_idea_heading']) ? $pro_options['custom_idea_heading'] : '';
 
-    $content = '<input type="checkbox" name="wp_roadmap_pro_settings[hide_custom_idea_heading]" value="1" ' . $hide_submit_idea_heading_checked . ' />';
-    $content .= '<br/><label for="custom_idea_heading">New Heading:</label>';
+    $content = '<input type="checkbox" name="wp_roadmap_pro_settings[hide_custom_idea_heading]" id="hide_custom_idea_heading" value="1" ' . $hide_submit_idea_heading_checked . ' />';
+    $content .= '<br/>';
+
+    // Add JavaScript to toggle the visibility of the label and input field based on checkbox state
+    $content .= '<script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var checkbox = document.getElementById("hide_custom_idea_heading");
+            var label = document.querySelector("label[for=custom_idea_heading]");
+            var input = document.querySelector("input[name=\'wp_roadmap_pro_settings[custom_idea_heading]\']");
+
+            checkbox.addEventListener("change", function() {
+                if (checkbox.checked) {
+                    label.style.display = "none";
+                    input.style.display = "none";
+                } else {
+                    label.style.display = "inline";
+                    input.style.display = "inline";
+                }
+            });
+
+            // Initialize visibility based on the initial checkbox state
+            if (checkbox.checked) {
+                label.style.display = "none";
+                input.style.display = "none";
+            }
+        });
+    </script>';
+
+    $content .= '<label for="custom_idea_heading">New Heading:</label>';
     $content .= '<input type="text" name="wp_roadmap_pro_settings[custom_idea_heading]" value="' . esc_attr($new_submit_idea_heading) . '" />';
 
     return $content;
