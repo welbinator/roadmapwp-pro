a7b23171b9b80e8b50c1330532f8a9ae41f094b6
James Welbes <james.welbes@gmail.com>
2023-12-08 20:52:12 -0600

    fix add taxonomy page refresh

--- app/settings/custom-taxonomies/custom-taxonomies.php
+++ app/settings/custom-taxonomies/custom-taxonomies.php
@@ -10,57 +10,56 @@ add_filter('wp_roadmap_pro_add_taxonomy_feature', 'wp_roadmap_pro_custom_taxonom
 function wp_roadmap_pro_custom_taxonomy_content() {
     ob_start();
 
+    
+    // Flag to trigger JavaScript redirection
+    $should_redirect = false;
+
     // Handle taxonomy deletion
     if (isset($_GET['action'], $_GET['taxonomy'], $_GET['_wpnonce']) && $_GET['action'] == 'delete') {
         if (wp_verify_nonce($_GET['_wpnonce'], 'delete_taxonomy_' . $_GET['taxonomy'])) {
             $taxonomies = get_option('wp_roadmap_custom_taxonomies', array());
             unset($taxonomies[$_GET['taxonomy']]);
             update_option('wp_roadmap_custom_taxonomies', $taxonomies);
-            echo '<div class="notice notice-success is-dismissible"><p>Taxonomy deleted successfully.</p></div>';
         }
     }
 
-    // Check if the form has been submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['wp_roadmap_pro_nonce'], $_POST['taxonomy_slug'])) {
+     // Check if the form has been submitted
+     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['wp_roadmap_pro_nonce'], $_POST['taxonomy_slug'])) {
         if (wp_verify_nonce($_POST['wp_roadmap_pro_nonce'], 'wp_roadmap_pro_add_taxonomy')) {
             $taxonomy_slug = sanitize_key($_POST['taxonomy_slug']);
             $taxonomy_singular = sanitize_text_field($_POST['taxonomy_singular']);
             $taxonomy_plural = sanitize_text_field($_POST['taxonomy_plural']);
-            $public = isset($_POST['public']) && $_POST['public'] === '1';
+            $public = (isset($_POST['public']) && $_POST['public'] === '1');
 
             $labels = array(
                 'name' => $taxonomy_plural,
                 'singular_name' => $taxonomy_singular,
-                // Add other labels as needed
+                // ... other labels as needed ...
             );
 
             $taxonomy_data = array(
                 'labels' => $labels,
                 'public' => $public,
-                // Add other taxonomy properties as needed
+                'hierarchical' => false,
                 'show_ui' => true,
-                'show_in_menu' => true,
-                'show_in_nav_menus' => true,
                 'show_in_rest' => true,
+                'show_admin_column' => true,
+                'query_var' => true,
+                'rewrite' => array('slug' => $taxonomy_slug),
             );
 
-            if (!taxonomy_exists($taxonomy_slug)) {
-                register_taxonomy($taxonomy_slug, 'idea', $taxonomy_data);
-                $custom_taxonomies = get_option('wp_roadmap_custom_taxonomies', array());
-                $custom_taxonomies[$taxonomy_slug] = $taxonomy_data;
-                update_option('wp_roadmap_custom_taxonomies', $custom_taxonomies);
-                echo '<div class="notice notice-success is-dismissible"><p>Taxonomy created successfully.</p></div>';
-            } else {
-                echo '<div class="notice notice-error is-dismissible"><p>Taxonomy already exists.</p></div>';
-            }
-        } else {
-            echo '<div class="notice notice-error is-dismissible"><p>Nonce verification failed.</p></div>';
+            register_taxonomy($taxonomy_slug, 'idea', $taxonomy_data);
+
+            $custom_taxonomies = get_option('wp_roadmap_custom_taxonomies', array());
+            $custom_taxonomies[$taxonomy_slug] = $taxonomy_data;
+            update_option('wp_roadmap_custom_taxonomies', $custom_taxonomies);
+
+            $should_redirect = true;
         }
     }
 
     ?>
     <div class="wrap">
-        <h1>Add Custom Taxonomy</h1>
         <form action="" method="post">
             <?php wp_nonce_field('wp_roadmap_pro_add_taxonomy', 'wp_roadmap_pro_nonce'); ?>
             <ul class="flex-outer">
@@ -88,14 +87,18 @@ function wp_roadmap_pro_custom_taxonomy_content() {
                 </li>
             </ul>
         </form>
+        <?php if ($should_redirect): ?>
+            <script type="text/javascript">
+                window.location.href = "<?php echo esc_url(admin_url('admin.php?page=wp-roadmap-taxonomies')); ?>";
+            </script>
+        <?php endif; ?>
     </div>
     <?php
 
-    // Retrieve and display existing custom taxonomies
+    // Retrieve and display existing taxonomies
     $custom_taxonomies = get_option('wp_roadmap_custom_taxonomies', array());
     echo '<h2>Existing Taxonomies</h2>';
     if (!empty($custom_taxonomies)) {
-       
         echo '<ul>';
         foreach ($custom_taxonomies as $taxonomy_slug => $taxonomy_data) {
             $delete_link = wp_nonce_url(
@@ -109,3 +112,4 @@ function wp_roadmap_pro_custom_taxonomy_content() {
 
     return ob_get_clean();
 }
+
