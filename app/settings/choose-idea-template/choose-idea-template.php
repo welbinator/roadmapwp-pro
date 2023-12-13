f1484056c0c39591c54edfbd402bc1e90f3feaf7
James Welbes <james.welbes@gmail.com>
2023-12-13 11:09:10 -0600

    add choose template or page feature

--- app/settings/choose-idea-template/choose-idea-template.php
+++ app/settings/choose-idea-template/choose-idea-template.php
@@ -4,46 +4,89 @@
  */
 
 // Hook into the settings page of the free version to add the choose template setting
-add_filter('wp_roadmap_choose_idea_template_setting', 'wp_roadmap_pro_choose_idea_template_setting');
+add_filter('wp_roadmap_single_idea_template_setting', 'wp_roadmap_pro_single_idea_template_setting');
 
-function wp_roadmap_pro_choose_idea_template_setting($content) {
-   // Fetch the current settings
-   $options = get_option('wp_roadmap_pro_settings', []);
-    
-   // Set 'custom' as default value if not set
-   $chosen_template = isset($options['chosen_idea_template']) ? $options['chosen_idea_template'] : 'custom';
+function wp_roadmap_pro_single_idea_template_setting($content) {
+    $options = get_option('wp_roadmap_settings', []);
+    $chosen_template = isset($options['single_idea_template']) ? $options['single_idea_template'] : 'plugin';
 
-    // Create the HTML for the dropdown
-    $html = '<select name="wp_roadmap_pro_settings[chosen_idea_template]">';
-    $templates = ['default' => 'Default Theme Template', 'custom' => 'Custom Plugin Template'];
+    $html = '<select name="wp_roadmap_settings[single_idea_template]" id="wp_roadmap_single_idea_template">';
+    $templates = ['plugin' => 'Plugin Template', 'page' => 'Choose Page'];
     foreach ($templates as $value => $label) {
         $selected = selected($chosen_template, $value, false);
         $html .= "<option value='{$value}' {$selected}>{$label}</option>";
     }
     $html .= '</select>';
 
+    // JavaScript to toggle the visibility of the 'Set page for single idea' setting
+    $html .= '<script type="text/javascript">
+        jQuery(document).ready(function($) {
+            function togglePageSetting() {
+                var selectedTemplate = $("#wp_roadmap_single_idea_template").val();
+                if (selectedTemplate === "page") {
+                    $("#single_idea_page_setting").show();
+                } else {
+                    $("#single_idea_page_setting").hide();
+                }
+            }
+            togglePageSetting();
+            $("#wp_roadmap_single_idea_template").change(togglePageSetting);
+        });
+    </script>';
+
     return $html;
 }
+
 
 // Save the setting when the settings form is submitted
 add_action('admin_init', 'wp_roadmap_pro_register_template_settings');
 
 function wp_roadmap_pro_register_template_settings() {
-    register_setting('wp_roadmap_settings', 'wp_roadmap_pro_settings');
+    register_setting('wp_roadmap_settings', 'wp_roadmap_settings');
 }
 
 // Implement the template choice functionality
-add_filter('template_include', 'wp_roadmap_pro_template_include');
-
 function wp_roadmap_pro_template_include($template) {
     if (is_singular('idea')) {
-        $options = get_option('wp_roadmap_pro_settings', []);
-        if (isset($options['chosen_idea_template']) && $options['chosen_idea_template'] === 'custom') {
-            $custom_template = locate_template('single-idea.php');
-            if (!empty($custom_template)) {
-                return $custom_template;
+        $options = get_option('wp_roadmap_settings', []);
+
+        if (isset($options['single_idea_template'])) {
+            if ($options['single_idea_template'] === 'plugin') {
+                $plugin_template = plugin_dir_path(__FILE__) . 'app/templates/template-single-idea.php';
+                if (file_exists($plugin_template)) {
+                    return $plugin_template;
+                }
+            } elseif ($options['single_idea_template'] === 'page' && isset($options['single_idea_page'])) {
+                $page_id = $options['single_idea_page'];
+                $page_link = get_permalink($page_id);
+                if ($page_link) {
+                    wp_redirect($page_link);
+                    exit;
+                }
             }
         }
     }
     return $template;
 }
+add_filter('template_include', 'wp_roadmap_pro_template_include');
+
+
+function wp_roadmap_pro_handle_single_idea_redirection() {
+    if (is_singular('idea')) {
+        $options = get_option('wp_roadmap_settings', []);
+
+        if (isset($options['single_idea_template']) && $options['single_idea_template'] === 'page' && isset($options['single_idea_page'])) {
+            $page_id = $options['single_idea_page'];
+            $page_link = add_query_arg('idea_id', get_queried_object_id(), get_permalink($page_id));
+            if ($page_link) {
+                wp_redirect($page_link);
+                exit;
+            }
+        }
+    }
+}
+add_action('template_redirect', 'wp_roadmap_pro_handle_single_idea_redirection');
+
+
+
+
