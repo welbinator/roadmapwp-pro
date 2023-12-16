f321e1bec20f5643a492b7c8b6696a3eca25aa8c
James Welbes <james.welbes@gmail.com>
2023-12-16 14:06:56 -0600

    update block placeholder text

--- pro/blocks/blocks.js
+++ pro/blocks/blocks.js
@@ -6,7 +6,7 @@ registerBlockType('wp-roadmap-pro/display-ideas', {
     title: __('Display Ideas', 'wp-roadmap-pro'),
     category: 'common',
     edit: function() {
-        return createElement('p', {}, __('Display Ideas Placeholder', 'wp-roadmap-pro'));
+        return createElement('p', {}, __('Display Ideas | This block will display your published ideas', 'wp-roadmap-pro'));
     },
     save: function() {
         return null; // Render via PHP
@@ -17,7 +17,7 @@ registerBlockType('wp-roadmap-pro/new-idea-form', {
     title: __('New Idea Form', 'wp-roadmap-pro'),
     category: 'common',
     edit: function() {
-        return createElement('p', {}, __('New Idea Form Placeholder', 'wp-roadmap-pro'));
+        return createElement('p', {}, __('New Idea Form | This block displays the form users will use to submit new ideas', 'wp-roadmap-pro'));
     },
     save: function() {
         return null; // Render via PHP
@@ -28,7 +28,7 @@ registerBlockType('wp-roadmap-pro/roadmap', {
     title: __('Roadmap', 'wp-roadmap-pro'),
     category: 'common',
     edit: function() {
-        return createElement('p', {}, __('Roadmap Placeholder', 'wp-roadmap-pro'));
+        return createElement('p', {}, __('Roadmap | This block displays your roadmap', 'wp-roadmap-pro'));
     },
     save: function() {
         return null; // Render via PHP
