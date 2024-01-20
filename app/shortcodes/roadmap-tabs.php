9fb20d0a10a60d1f5f01278c3e0b8efd4ac68fa7
James Welbes <james.welbes@gmail.com>
2024-01-20 12:39:32 -0600

    namespace roadmap tabs shortcode

--- app/shortcodes/roadmap-tabs.php
+++ app/shortcodes/roadmap-tabs.php
@@ -1,5 +1,20 @@
 <?php
-function wp_roadmap_pro_roadmap_tabs_shortcode( $atts ) {
+/**
+ * RoadMapWP Pro Plugin - Roadmap Tabs Shortcode
+ *
+ * This file contains the shortcode [roadmap_tabs] which is used to display
+ * a tabbed interface for roadmap statuses in the RoadMapWP Pro plugin.
+ */
+
+namespace RoadMapWP\Pro;
+
+/**
+ * Shortcode to display roadmap tabs.
+ *
+ * @param array $atts Shortcode attributes.
+ * @return string HTML output for the roadmap tabs.
+ */
+function roadmap_tabs_shortcode( $atts ) {
 	$atts = shortcode_atts(
 		array(
 			'status'        => '',
@@ -140,4 +155,4 @@ function wp_roadmap_pro_roadmap_tabs_shortcode( $atts ) {
 
 	return ob_get_clean(); // Return the buffered output
 }
-add_shortcode( 'roadmap_tabs', 'wp_roadmap_pro_roadmap_tabs_shortcode' );
+add_shortcode( 'roadmap_tabs', __NAMESPACE__ . '\\roadmap_tabs_shortcode' );
