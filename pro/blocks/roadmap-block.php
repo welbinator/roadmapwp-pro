7dea2d0994c3fc9cfe86d15cfc40150491f2288d
James Welbes <james.welbes@gmail.com>
2024-02-27 00:38:51 -0600

    phpcs roadmap block

--- pro/blocks/roadmap-block.php
+++ pro/blocks/roadmap-block.php
@@ -2,6 +2,8 @@
 /**
  * This file handles the registration and rendering of the 'Roadmap Block' for the RoadMapWP Pro plugin.
  * It includes functions to register the block and its script, as well as to render the block in the editor.
+ *
+ * @package RoadMapWP\Pro\Blocks\NewIdeaForm
  */
 
 namespace RoadMapWP\Pro\Blocks\Roadmap;
@@ -12,7 +14,7 @@ use RoadMapWP\Pro\Admin\Functions;
  */
 function register_block() {
 
-	// Register the block
+	// Register the block.
 	$roadmap_block_path = plugin_dir_path( dirname( __DIR__ ) ) . 'build/roadmap-block';
 	register_block_type_from_metadata(
 		$roadmap_block_path,
@@ -47,22 +49,20 @@ add_action( 'init', __NAMESPACE__ . '\register_block' );
 function block_render( $attributes ) {
 
 	if ( ! empty( $attributes['onlyLoggedInUsers'] ) && ! is_user_logged_in() ) {
-		// Return an empty string or a specific message indicating the need to log in
 		return '';
 	}
 
-	// Check if selectedStatuses attribute is set and is an array
+	// Check if selectedStatuses attribute is set and is an array.
 	if ( isset( $attributes['selectedStatuses'] ) && is_array( $attributes['selectedStatuses'] ) ) {
 		$selected_statuses = array_keys( array_filter( $attributes['selectedStatuses'] ) );
 	} else {
-		// If no statuses are selected, you can choose to return nothing or handle it differently
 		return '<p>No statuses selected.</p>';
 	}
 
-	// Check the status filter attribute
-	$include_pending = isset( $attributes['statusFilter'] ) && $attributes['statusFilter'] === 'include_pending';
+	// Check the status filter attribute.
+	$include_pending = isset( $attributes['statusFilter'] ) && 'include_pending' === $attributes['statusFilter'];
 
-	// Retrieve color settings
+	// Retrieve color settings.
 	$options                = get_option( 'wp_roadmap_settings' );
 	$vote_button_bg_color   = isset( $options['vote_button_bg_color'] ) ? $options['vote_button_bg_color'] : '#ff0000';
 	$vote_button_text_color = isset( $options['vote_button_text_color'] ) ? $options['vote_button_text_color'] : '#ffffff';
@@ -70,35 +70,36 @@ function block_render( $attributes ) {
 	$filter_tags_text_color = isset( $options['filter_tags_text_color'] ) ? $options['filter_tags_text_color'] : '#ffffff';
 
 	$num_statuses  = count( $selected_statuses );
-	$md_cols_class = 'md:grid-cols-' . ( $num_statuses > 3 ? 3 : $num_statuses ); // Set to number of statuses, but max out at 3
+	$md_cols_class = 'md:grid-cols-' . ( $num_statuses > 3 ? 3 : $num_statuses );
 	$lg_cols_class = 'lg:grid-cols-' . ( $num_statuses > 4 ? 4 : $num_statuses );
 	$xl_cols_class = 'xl:grid-cols-' . $num_statuses;
 	ob_start();
 
-	// Always include 'idea-tag' taxonomy
+	// Always include 'idea-tag' taxonomy.
 	$taxonomies = array( 'idea-tag' );
 
-	// Include custom taxonomies
+	// Include custom taxonomies.
 	$custom_taxonomies = get_option( 'wp_roadmap_custom_taxonomies', array() );
 	$taxonomies        = array_merge( $taxonomies, array_keys( $custom_taxonomies ) );
 
-	// Exclude 'status' taxonomy
+	// Exclude 'status' taxonomy.
 	$exclude_taxonomies = array( 'status' );
 	$taxonomies         = array_diff( $taxonomies, $exclude_taxonomies );
 	?>
 
 	<div class="roadmap_wrapper container mx-auto">
-		<div class="roadmap-columns grid gap-4 <?php echo $md_cols_class; ?> <?php echo $lg_cols_class; ?> <?php echo $xl_cols_class; ?>">
+	<div class="roadmap-columns grid gap-4 <?php echo esc_attr( $md_cols_class ); ?> <?php echo esc_attr( $lg_cols_class ); ?> <?php echo esc_attr( $xl_cols_class ); ?>">
 			<?php
 			foreach ( $selected_statuses as $status_slug ) :
 				$term = get_term_by( 'slug', $status_slug, 'status' );
 				if ( ! $term ) {
-					continue; // Skip if term not found
+					continue;
 				}
 
 				$args = array(
 					'post_type'      => 'idea',
 					'posts_per_page' => -1,
+					// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_tax_query
 					'tax_query'      => array(
 						array(
 							'taxonomy' => 'status',
@@ -108,7 +109,7 @@ function block_render( $attributes ) {
 					),
 				);
 
-				// Include pending review ideas if selected
+				// Include pending review ideas if selected.
 				if ( $include_pending ) {
 					$args['post_status'] = array( 'publish', 'pending' );
 				}
@@ -125,14 +126,14 @@ function block_render( $attributes ) {
 							$idea_id    = get_the_ID();
 							$vote_count = intval( get_post_meta( $idea_id, 'idea_votes', true ) );
 							$idea_class = Functions\get_idea_class_with_votes( $idea_id );
-							// Check post status if including pending reviews
+							// Check post status if including pending reviews.
 							if ( ! $include_pending && get_post_status() !== 'publish' ) {
 								continue;
 							}
 							?>
 							<div class="wp-roadmap-idea border bg-card text-card-foreground rounded-lg shadow-lg overflow-hidden m-2 <?php echo esc_attr( $idea_class ); ?>">
 								<div class="p-6">
-									<h4 class="idea-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
+								<h4 class="idea-title"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h4>
 									<p class="text-gray-500 mt-2 mb-0 text-sm"><?php echo esc_html( get_the_date() ); ?></p>
 									<div class="flex flex-wrap space-x-2 mt-2 idea-tags">
 									<?php
@@ -148,7 +149,7 @@ function block_render( $attributes ) {
 									?>
 									</div>
 									
-									<div class="idea-excerpt mt-4"><?php echo get_the_excerpt(); ?> <a class="text-blue-500 hover:underline" href="<?php the_permalink(); ?>" rel="ugc">read more...</a></div>
+									<div class="idea-excerpt mt-4"><?php echo esc_html( get_the_excerpt() ); ?> <a class="text-blue-500 hover:underline" href="<?php echo esc_url( get_permalink() ); ?>" rel="ugc">read more...</a></div>
 									<div class="flex items-center justify-start mt-6 gap-6">
 									
 									<div class="flex items-center idea-vote-box" data-idea-id="<?php echo intval( $idea_id ); ?>">
@@ -168,24 +169,29 @@ function block_render( $attributes ) {
 												<path d="M7 10v12"></path>
 												<path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2h0a3.13 3.13 0 0 1 3 3.88Z"></path>
 											</svg>
-											<div class="text-gray-600 ml-2 idea-vote-count" style="color: <?php echo esc_attr( $vote_button_text_color ); ?>;"><?php echo $vote_count; ?></div>
+											<div class="text-gray-600 ml-2 idea-vote-count" style="color: <?php echo esc_attr( $vote_button_text_color ); ?>;"><?php echo esc_html( $vote_count ); ?></div>
 										</button>
 										
 									</div>
 								</div>
 								</div>
-								<?php if ( current_user_can( 'administrator' ) ) : ?>
+								<?php if ( current_user_can( 'manage_options' ) ) : ?>
 									<div class="p-6 bg-gray-200">
 										<h6 class="text-center">Admin only</h6>
 										<form class="idea-status-update-form" data-idea-id="<?php echo intval( $idea_id ); ?>">
 											<select multiple class="status-select" name="idea_status[]">
 												<?php
-												$statuses         = get_terms( 'status', array( 'hide_empty' => false ) );
+												$statuses         = get_terms(
+													array(
+														'taxonomy' => 'status',
+														'hide_empty' => false,
+													)
+												);
 												$current_statuses = wp_get_post_terms( $idea_id, 'status', array( 'fields' => 'slugs' ) );
 
 												foreach ( $statuses as $status ) {
-													$selected = in_array( $status->slug, $current_statuses ) ? 'selected' : '';
-													echo '<option value="' . esc_attr( $status->slug ) . '" ' . $selected . '>' . esc_html( $status->name ) . '</option>';
+													$selected = in_array( $status->slug, $current_statuses, true ) ? 'selected' : '';
+													echo '<option value="' . esc_attr( $status->slug ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $status->name ) . '</option>';
 												}
 												?>
 											</select>
